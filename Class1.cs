2e89da876d81164b46d635d2533126e40e714965
Agrupamento de Escolas Maximinos <aemaximinosweb@gmail.com>
2020-03-04 13:07:54 +0000

    Adicionar arquivos de projeto.

---
+++ Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clicar
+{
+    class Class1
+    {
+        private static string _path = Application.StartupPath + "\\Resources\\Texto.txt";
+        private int _pontos;
+        private static int tmp = 30;
+        private int _tempo = tmp;
+
+        public void Move(object sender)
+        {
+            if (_tempo > 0)
+            {
+                Random r = new Random();
+                int _x = int.Parse(r.Next(700).ToString());
+                int _y = int.Parse(r.Next(250).ToString()) + 100;
+                Point pt = new Point(_x, _y);
+                Button b = (Button)sender;
+                b.Location = pt;
+            }
+        }
+
+        public string Conta()
+        {
+            if (_tempo > 0)
+            {
+                _pontos += 1;
+            }
+            return _pontos.ToString();
+        }
+
+        public string Temp()
+        {
+            if (_tempo > 0)
+            {
+                _tempo -= 1;
+            }
+            return _tempo.ToString();
+        }
+
+        public string ResP()
+        {
+            _pontos = 0;
+            return (_pontos).ToString();
+        }
+
+        public string ResT()
+        {
+            _tempo = tmp;
+            return (_tempo).ToString();
+        }
+
+        public void ResPos(object sender)
+        {
+            Point pt = new Point(350, 175);
+            Button b = (Button)sender;
+            b.Location = pt;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clicar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Class1 Classe = new Class1();
+
+        Class1 c = new Class1();
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            label5.Text = c.Conta();
+            timer1.Start();
+            c.Move(sender);
+        }
+
+        private void Timer1_Tick_1(object sender, EventArgs e)
+        {
+            label4.Text = c.Temp();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            c.ResPos(button1);
+            label1.Text = c.ResP();
+            label5.Text = c.ResT();
+        }
+    }
+}
